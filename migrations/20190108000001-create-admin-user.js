e9f95f728f5dc833dc004b29611281a48650b4dc
Xavier Sellier <xsellier@gmail.com>
2019-01-08 19:32:04 -0500

    Bugfix: User model uses an undefined variable Feature: Add request validation (Joi) Feature: Add HTTP error reporting (boom)

---
+++ migrations/20190108000001-create-admin-user.js
@@ -0,0 +1,23 @@
+exports.up = function (db) {
+  return new Promise((resolve, reject) => {
+    db.insert('account', ['hashed_password'], ['907f64babcc0a7057eae636ee1a732969c709948790a93fc2c85791d660996b9'], (err) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
+
+exports.down = function (db) {
+  return new Promise((resolve, reject) => {
+    db.runSql('TRUNCATE account'), (err) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    }
+  })
+}
